f8fb13a6af8c3dd22d926e99036c3f142af2e254
panchangcheng <panchangcheng@ecpark.cn>
2021-04-29 17:22:59 +0800

    optimize workloads router

--- client/components/+tekton-taskrun/taskrun.tsx
+++ client/components/+tekton-taskrun/taskrun.tsx
@@ -145,11 +145,11 @@ export class TaskRuns extends React.Component<Props> {
             className: "namespace",
             sortBy: sortBy.namespace,
           },
-          {
-            title: <Trans>OwnerNamespace</Trans>,
-            className: "ownernamespace",
-            sortBy: sortBy.ownernamespace,
-          },
+          // {
+          //   title: <Trans>OwnerNamespace</Trans>,
+          //   className: "ownernamespace",
+          //   sortBy: sortBy.ownernamespace,
+          // },
           { title: <Trans>Steps</Trans>, className: "steps" },
           { title: <Trans>Timeout</Trans>, className: "timeout" },
           { title: <Trans>Age</Trans>, className: "age", sortBy: sortBy.age },
@@ -157,7 +157,7 @@ export class TaskRuns extends React.Component<Props> {
         renderTableContents={(taskRun: TaskRun) => [
           taskRun.getName(),
           taskRun.getNs(),
-          taskRun.getOwnerNamespace(),
+          // taskRun.getOwnerNamespace(),
           this.renderSteps(taskRun),
           taskRun.spec.timeout,
           taskRun.getAge(),
